7ce92c47d67d020524af4b80cb3a61a846a3ae2b
naokotani <nao.kotani.i@finc.com>
2018-09-24 22:40:36 +0900

    initialize

---
+++ src/account.ts
@@ -0,0 +1,31 @@
+import { updateTokensAndTableAsync } from './tokens';
+import { Sdk } from './Sdk';
+import { EventName } from '@radarrelay/sdk';
+
+// Selectors
+const activeAddressSelector = '#active-address';
+const loaderSelector = '.loader';
+
+
+/**
+ * Watch the active address. Update the balances
+ * and address in the UI id changed
+ */
+export function watchActiveAddress() {
+  // Set address in UI
+  $(activeAddressSelector).text(Sdk.Instance.account.address);
+  console.log(Sdk.Instance);
+
+  Sdk.Instance.events.on(EventName.AddressChanged, async (address: string) => {
+    $(activeAddressSelector).text(address || 'None');
+
+    // Show Loader
+    $(loaderSelector).show();
+
+    // Update balances and allowances
+    await updateTokensAndTableAsync();
+
+    // Hide Loader
+    $(loaderSelector).hide();
+  });
+}
